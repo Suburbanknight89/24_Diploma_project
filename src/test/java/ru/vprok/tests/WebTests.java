008d1818fce4e066055e85a7a86b3763d77a6182
suburbanknight89 <lumenteam@gmail.com>
2021-10-24 20:19:50 +0300

    3 add 2 web tests

--- src/test/java/ru/vprok/tests/WebTests.java
+++ src/test/java/ru/vprok/tests/WebTests.java
@@ -1,6 +1,7 @@
 package ru.vprok.tests;
 
 import com.codeborne.selenide.selector.ByText;
+import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -22,8 +23,9 @@ public class WebTests extends TestBase{
 	@Test
 	@DisplayName("Vprok.ru tests 1")
 	@Story("Отображение 'Товар недели' на главной странице")
-	@Microservice("Billing")
+	@Microservice("Search")
 	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
 	@JiraIssues({@JiraIssue("HOMEWORK-250")})
 	void assertProductOFTheWeekTest() {
 		step("open https://www.vprok.ru/", () -> {
@@ -38,8 +40,9 @@ public class WebTests extends TestBase{
 	@Test
 	@DisplayName("Vprok.ru tests 2")
 	@Story("Отображение 'Суперцены недели' на главной странице")
-	@Microservice("Billing")
+	@Microservice("Search")
 	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
 	@JiraIssues({@JiraIssue("HOMEWORK-250")})
 	void assertSuperPricesTest() {
 		step("open https://www.vprok.ru/", () -> {
@@ -54,8 +57,9 @@ public class WebTests extends TestBase{
 	@Test
 	@DisplayName("Vprok.ru tests 3")
 	@Story("Проверка ссылки 'Упаковкой дешевле'")
-	@Microservice("Billing")
+	@Microservice("Search")
 	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
 	@JiraIssues({@JiraIssue("HOMEWORK-250")})
 	void assertCatalogueLink() {
 		step("open https://www.vprok.ru/", () -> {
@@ -72,8 +76,9 @@ public class WebTests extends TestBase{
 	@Test
 	@DisplayName("Vprok.ru tests 4")
 	@Story("Проверка ссылки 'Зоотовары'")
-	@Microservice("Billing")
+	@Microservice("Search")
 	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
 	@JiraIssues({@JiraIssue("HOMEWORK-250")})
 	void assertCatalogueLinkZoo() {
 		step("open https://www.vprok.ru/", () -> {
@@ -90,8 +95,9 @@ public class WebTests extends TestBase{
 	@Test
 	@DisplayName("Vprok.ru tests 5")
 	@Story("Проверка заголовка 'Зоотовары'")
-	@Microservice("Billing")
+	@Microservice("Search")
 	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
 	@JiraIssues({@JiraIssue("HOMEWORK-250")})
 	void assertZooHeader() {
 		step("open https://www.vprok.ru/", () -> {
@@ -105,4 +111,47 @@ public class WebTests extends TestBase{
 		});
 	}
 
+		@Test
+		@DisplayName("Vprok.ru tests 6")
+		@Story("Проверка поиска по продукту 'молоко'")
+		@Microservice("Search")
+		@Tags({@Tag("web")})
+		@Owner("ZaytsevE")
+		@JiraIssues({@JiraIssue("HOMEWORK-250")})
+		void searchMilktest() {
+			step("open https://www.vprok.ru/", () -> {
+				open("https://www.vprok.ru/");
+			});
+
+			step("check Catalogue", () -> {
+				$(byXpath("(//input[@name='search'])[2]")).val("молоко").pressEnter();
+				$("#ui-id-31 .xf-product-title__link").
+						shouldHave(text("Молоко"));
+			});
+		}
+
+	@Test
+	@DisplayName("Vprok.ru tests 7")
+	@Story("Проверка отправки смс кода при регистрации нового клиетна")
+	@Microservice("Registration")
+	@Tags({@Tag("web")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void searchMilktest1() {
+		step("open https://www.vprok.ru/", () -> {
+			open("https://www.vprok.ru/");
+		});
+
+		step("check Catalogue", () -> {
+			$(".xfnew-nav__dropdown-button").click();
+			$(byText("Регистрация Интернет-Магазин")).click();
+			$(byName("phone")).val("+9999999999");
+			$(byName("name")).val("9999");
+			$(byName("email")).val("9999@9999.ru");
+			$(".xf-registration__btn").click();
+			$(".xf-registration-pin__timer").shouldHave(text("Отправить"));
+
+
+		});
+	}
 }
