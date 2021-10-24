83a808cee244858774519625ca19c55c2c7358a2
suburbanknight89 <lumenteam@gmail.com>
2021-10-24 20:19:25 +0300

    2 add reqres in tests

--- src/test/java/ru/vprok/tests/ApiTests.java
+++ src/test/java/ru/vprok/tests/ApiTests.java
@@ -1,32 +1,118 @@
 package ru.vprok.tests;
 
-		import ru.vprok.allure.Layer;
-		import ru.vprok.allure.Lead;
-		import ru.vprok.allure.Microservice;
-		import io.qameta.allure.Feature;
-		import io.qameta.allure.Owner;
-		import io.qameta.allure.Story;
-		import io.restassured.path.json.JsonPath;
-		import io.restassured.response.Response;
-		import org.junit.jupiter.api.DisplayName;
-		import org.junit.jupiter.api.Tag;
-		import org.junit.jupiter.api.Tags;
-		import org.junit.jupiter.api.Test;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+import ru.vprok.allure.JiraIssue;
+import ru.vprok.allure.JiraIssues;
 
-		import java.util.ArrayList;
-		import java.util.Map;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
 
-		import static io.qameta.allure.Allure.step;
-		import static io.restassured.RestAssured.given;
-		import static org.hamcrest.Matchers.hasItems;
-		import static org.hamcrest.core.Is.is;
 
-@Layer("rest")
-@Lead("qa")
-@Owner("ZaytsevE")
-@Feature("User")
 public class ApiTests {
+	@BeforeAll
+	static void setup() {
+		RestAssured.baseURI = "https://reqres.in";
+	}
+
+	@Test
+	@DisplayName("reqres.in tests create user")
+	@Story("Создание нового пользователя")
+	@Tags({@Tag("api")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void usersCreateNew() {
+		given ()
+				.contentType (JSON)
+				.body ("{\"name\": \"suburban\"," +
+						"\"job\": \"qa\"}")
+				.when ()
+				.post ("https://reqres.in/api/users")
+				.then ()
+				.statusCode (201)
+				.body ("name", is ("suburban"), "job", is ("qa"))
+				.extract().response().asString();
+	}
+
+
+
+	@Test
+	@DisplayName("reqres.in tests get info")
+	@Story("Запрос пользовательских данных")
+	@Tags({@Tag("api")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void usersGetInfo() {
+		given()
+				.contentType(JSON)
+				.when()
+				.get("https://reqres.in/api/users/6")
+				.then()
+				.statusCode(200)
+				.body("data.email", is("tracey.ramos@reqres.in"))
+				.body("data.last_name", is("Ramos"));
+	}
+
+
+
+	@Test
+	@DisplayName("reqres.in tests support.url")
+	@Story("Проверка ответа в блоке support.Url")
+	@Tags({@Tag("api")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void usersListGetFindUrl () {
+		given()
+				.contentType(JSON)
+				.when()
+				.get("https://reqres.in/api/users?page=3")
+				.then()
+				.statusCode(200)
+				.body("support.url", is("https://reqres.in/#support-heading"));
+	}
+
+
+	@Test
+	@DisplayName("reqres.in ussuccesfull user creation")
+	@Story("Неуспешное создание нового пользователя(негативный)")
+	@Tags({@Tag("api")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void unsuccesfullCreateUser() {
+		given()
+				.contentType(JSON)
+				.body("{\n" +
+						"    \"name\": \"morpheus\",\n" +
+						"}")
+				.when()
+				.post("https://reqres.in/api/users")
+				.then()
+				.assertThat().statusCode(400);
+	}
+
+	@Test
+	@DisplayName("reqres.in succesfull registration")
+	@Story("Успешное создание нового пользователя(негативный)")
+	@Tags({@Tag("api")})
+	@Owner("ZaytsevE")
+	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	void registrationSuccessfulTest() {
+		given()
+				.contentType(JSON)
+				.body("{ \"email\": \"eve.holt@reqres.in\", " +
+						"\"password\": \"pistol\" }")
+				.when()
+				.post("/api/register")
+				.then()
+				.statusCode(200)
+				.body("id", is(4))
+				.body("token", is("QpwL5tke4Pnpja7X4"));
+	}
 }
+
 
 
 
