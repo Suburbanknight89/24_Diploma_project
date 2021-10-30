649319fc342ae4cb01a72ea1dc7f143742554eba
suburbanknight89 <lumenteam@gmail.com>
2021-10-30 21:10:22 +0300

    8 reformate code

--- src/test/java/ru/vprok/tests/ApiTests.java
+++ src/test/java/ru/vprok/tests/ApiTests.java
@@ -12,8 +12,8 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
-
 public class ApiTests {
+
 	@BeforeAll
 	static void setup() {
 		RestAssured.baseURI = "https://reqres.in";
@@ -23,31 +23,29 @@ public class ApiTests {
 	@DisplayName("reqres.in tests create user")
 	@Layer("api")
 	@Story("Создание нового пользователя")
-	@Tags({@Tag("api")})
+	@Tags({ @Tag("api") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void usersCreateNew() {
-		given ()
-				.contentType (JSON)
-				.body ("{\"name\": \"suburban\"," +
+		given()
+				.contentType(JSON)
+				.body("{\"name\": \"suburban\"," +
 						"\"job\": \"qa\"}")
-				.when ()
-				.post ("https://reqres.in/api/users")
-				.then ()
-				.statusCode (201)
-				.body ("name", is ("suburban"), "job", is ("qa"))
+				.when()
+				.post("https://reqres.in/api/users")
+				.then()
+				.statusCode(201)
+				.body("name", is("suburban"), "job", is("qa"))
 				.extract().response().asString();
 	}
-
-
 
 	@Test
 	@DisplayName("reqres.in tests get info")
 	@Layer("api")
 	@Story("Запрос пользовательских данных")
-	@Tags({@Tag("api")})
+	@Tags({ @Tag("api") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void usersGetInfo() {
 		given()
 				.contentType(JSON)
@@ -59,16 +57,14 @@ public class ApiTests {
 				.body("data.last_name", is("Ramos"));
 	}
 
-
-
 	@Test
 	@DisplayName("reqres.in tests support.url")
 	@Layer("api")
 	@Story("Проверка ответа в блоке support.Url")
-	@Tags({@Tag("api")})
+	@Tags({ @Tag("api") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
-	void usersListGetFindUrl () {
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
+	void usersListGetFindUrl() {
 		given()
 				.contentType(JSON)
 				.when()
@@ -78,14 +74,13 @@ public class ApiTests {
 				.body("support.url", is("https://reqres.in/#support-heading"));
 	}
 
-
 	@Test
 	@DisplayName("reqres.in ussuccesfull user creation")
 	@Layer("api")
 	@Story("Неуспешное создание нового пользователя(негативный)")
-	@Tags({@Tag("api")})
+	@Tags({ @Tag("api") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void unsuccesfullCreateUser() {
 		given()
 				.contentType(JSON)
@@ -102,9 +97,9 @@ public class ApiTests {
 	@DisplayName("reqres.in succesfull registration")
 	@Layer("api")
 	@Story("Успешное создание нового пользователя")
-	@Tags({@Tag("api")})
+	@Tags({ @Tag("api") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void registrationSuccessfulTest() {
 		given()
 				.contentType(JSON)
--- src/test/java/ru/vprok/tests/WebTests.java
+++ src/test/java/ru/vprok/tests/WebTests.java
@@ -1,6 +1,6 @@
 package ru.vprok.tests;
 
-import com.codeborne.selenide.ClickOptions;
+
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
@@ -8,31 +8,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import ru.vprok.allure.JiraIssue;
 import ru.vprok.allure.JiraIssues;
 import ru.vprok.allure.Layer;
 import ru.vprok.allure.Microservice;
-
-import static com.codeborne.selenide.ClickOptions.usingJavaScript;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
-public class WebTests extends TestBase{
+public class WebTests extends TestBase {
 
 	@Test
 	@DisplayName("Отображение 'Товар недели' на главной страниц")
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void assertProductOFTheWeekTest() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -48,9 +43,9 @@ public class WebTests extends TestBase{
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void assertSuperPricesTest() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -66,9 +61,9 @@ public class WebTests extends TestBase{
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void assertCatalogueLink() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -76,19 +71,18 @@ public class WebTests extends TestBase{
 
 		step("check that 'expensive if pack' lead to correct link", () -> {
 			$(".xfnew-header__catalog-button").click();
-					$(linkText("Упаковкой дешевле")).shouldHave(href("catalog/4924/upakovkoy-deshevle"));
+			$(linkText("Упаковкой дешевле")).shouldHave(href("catalog/4924/upakovkoy-deshevle"));
 		});
 	}
-
 
 	@Test
 	@DisplayName("Проверка что в заголовках блока зоотовары есть подзаголовок 'лакомства для кошек'")
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void assertCatalogueLinkZoo() {
 		step("open https://zoo.vprok.ru/", () -> {
 			open("https://zoo.vprok.ru/");
@@ -104,9 +98,9 @@ public class WebTests extends TestBase{
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void assertZooHeader() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -117,43 +111,42 @@ public class WebTests extends TestBase{
 			$(linkText("Зоотовары")).click();
 		});
 
-			step("check that 'Хиты продаж' contains in search results", () -> {
-				$(".xf-main-page__header-wrapper").shouldHave(text("Хиты продаж"));
+		step("check that 'Хиты продаж' contains in search results", () -> {
+			$(".xf-main-page__header-wrapper").shouldHave(text("Хиты продаж"));
 		});
 	}
 
-		@Test
-		@DisplayName("Проверка поиска по продукту 'молоко'")
-		@Layer("web")
-		@Story("vprok.ru tests")
-		@Microservice("Search")
-		@Tags({@Tag("web")})
-		@Owner("ZaytsevE")
-		@JiraIssues({@JiraIssue("HOMEWORK-250")})
-		void searchMilktest() {
-			step("open https://www.vprok.ru/", () -> {
-				open("https://www.vprok.ru/");
-			});
+	@Test
+	@DisplayName("Проверка поиска по продукту 'молоко'")
+	@Layer("web")
+	@Story("vprok.ru tests")
+	@Microservice("Search")
+	@Tags({ @Tag("web") })
+	@Owner("ZaytsevE")
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
+	void searchMilktest() {
+		step("open https://www.vprok.ru/", () -> {
+			open("https://www.vprok.ru/");
+		});
 
-			step("search 'milk'", () -> {
-				$(byXpath("(//input[@name='search'])[2]")).val("молоко").pressEnter();
-			});
+		step("search 'milk'", () -> {
+			$(byXpath("(//input[@name='search'])[2]")).val("молоко").pressEnter();
+		});
 
-			step("check that 'молоко' contains in search results", () -> {
-					$("#ui-id-31 .xf-product-title__link").
-							shouldHave(text("Молоко"));
-			});
-
-		}
+		step("check that 'молоко' contains in search results", () -> {
+			$("#ui-id-31 .xf-product-title__link").
+					shouldHave(text("Молоко"));
+		});
+	}
 
 	@Test
 	@DisplayName("Проверка отправки смс кода при регистрации нового клиента")
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Registration")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void searchMilktest1() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -167,21 +160,21 @@ public class WebTests extends TestBase{
 			$(byName("email")).val("9999@9999.ru");
 			$(".xf-registration__btn").click();
 			$(".xf-registration-pin__timer").shouldHave(text("Отправить"));
-			});
+		});
 
 		step("check that countdown begins", () -> {
-				$(".xf-registration-pin__timer").shouldHave(text("Отправить"));
-			});
-
+			$(".xf-registration-pin__timer").shouldHave(text("Отправить"));
+		});
 	}
+
 	@Test
 	@DisplayName("Проверка фильтрации в категории  'Чай, кофе, сахар'")
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void filterTeaTest() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -200,7 +193,6 @@ public class WebTests extends TestBase{
 			$(".selectboxit-option:nth-child(3)").shouldNotHave(text("кофе")).
 					shouldNotHave(text("какао")).shouldNotHave(text("сахар"));
 		});
-
 	}
 
 	@Test
@@ -208,9 +200,9 @@ public class WebTests extends TestBase{
 	@Layer("web")
 	@Story("vprok.ru tests")
 	@Microservice("Search")
-	@Tags({@Tag("web")})
+	@Tags({ @Tag("web") })
 	@Owner("ZaytsevE")
-	@JiraIssues({@JiraIssue("HOMEWORK-250")})
+	@JiraIssues({ @JiraIssue("HOMEWORK-250") })
 	void basketTest() {
 		step("open https://www.vprok.ru/", () -> {
 			open("https://www.vprok.ru/");
@@ -236,7 +228,7 @@ public class WebTests extends TestBase{
 		});
 
 		step("go to the basket", () -> {
-			$(".xfnew-header__cart-wrap",1).click();
+			$(".xfnew-header__cart-wrap", 1).click();
 		});
 
 		step("check that selected prodict is in the basket", () -> {
